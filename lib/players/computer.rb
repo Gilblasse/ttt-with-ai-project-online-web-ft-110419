d6a35f5abd621a4b0303ab2d23d3dd606a0c5b73
Nethelbert <gilblasse@gmail.com>
2019-11-30 17:39:52 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,7 @@ module Players
   class Computer < Player
     
     def move(board)
-      # board.cells.map.with_index(1){|v,i| v == " " ? "#{i}" : v}.grep(/\d/)
-       (1..9).to_a.sample.to_s
+      board.cells.map.with_index(1){|v,i| v == " " ? "#{i}" : v}.grep(/\d/).sample
     end
     
   end
